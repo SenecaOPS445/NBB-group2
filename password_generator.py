ab4c4a5fca644e4570ad3557c77a93a54f22d100
ZThompson1 <zacharygthompson@gmail.com>
2024-11-19 17:37:37 -0500

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+'''
+This is our rough draft for our password_generator.py file
+'''
+
+def load_config(filename="config.txt"):
+'''
+Reads and loads the configuration file, applying default values if no custom settings are provided
+'''
+def main():
+'''
+Main program flow, managing user input, random generation mode, and invoking other functions
+'''
+
+def get_user_keywords():
+'''
+Prompts the user to input keywords or defaults to random word selection based on settings
+'''
+  
+
+def generate_passphrase(keywords):
+'''
+Generates a passphrase using provided keywords or random words from a predefined list
+'''
+
+def add_special_characters(passphrase):
+'''
+Adds special characters to the passphrase for increased complexity
+'''
+  
+
+def randomize_cases(passphrase):
+'''
+Randomly adjusts the casing of words within the passphrase
+'''
+  
+
+def validate_passphrase(passphrase):
+'''
+Validates the generated passphrase against security criteria, such as length and character variety
+'''
+  
+
+  
+
+def display_passphrase(passphrase):
+'''
+Formats and securely displays the passphrase to the user
+'''
+
+
+if __name__ == '__main__':
